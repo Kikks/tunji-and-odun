05faca6349d123e1827b95a3024b595c80fa4897
Olufemi Okikioluwa <kikkyboysven@gmail.com>
2026-05-24 23:26:23 +0100

    chore: tailwind migation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://example.com",
   compressHTML: true,
   build: {
     inlineStylesheets: "auto",
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
